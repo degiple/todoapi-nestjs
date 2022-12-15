81ce2c37c79365994f9d9c6f7ace2ddbab46c5b5
degiple <k.takeshima@degiple.com>
2022-12-15 08:36:04 +0000

    Update configration for NestJS

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,17 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { Request } from 'express';
-import * as cookieParser from 'cookie-parser';
-import * as csurf from 'csurf';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
+import { Request } from 'express'
+import * as cookieParser from 'cookie-parser'
+import * as csurf from 'csurf'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix('api')
+
   app.enableCors({
     credentials: true,
     origin: ['http://localhost:3000'],
-  });
-  app.use(cookieParser());
+  })
+  app.use(cookieParser())
   app.use(
     csurf({
       cookie: {
@@ -20,10 +22,10 @@ async function bootstrap() {
         secure: false,
       },
       value: (req: Request) => {
-        return req.header('csrf-token');
+        return req.header('csrf-token')
       },
     }),
-  );
-  await app.listen(process.env.PORT || 3005);
+  )
+  await app.listen(process.env.PORT || 3005)
 }
-bootstrap();
+bootstrap()
